8907c58075920479d2bc253ff2deee8ae0a89c87
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-11-16 00:46:42 +0100

    feat: add footnote support with styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 import EleventyPluginOgImage from "eleventy-plugin-og-image";
 import markdownIt from "markdown-it";
 import markdownItLinkAttributes from "markdown-it-link-attributes";
+import markdownItFootnote from "markdown-it-footnote";
 import fs from "fs";
 import i18n from "eleventy-plugin-i18n/i18n.js";
 
@@ -59,15 +60,43 @@ export default async function (eleventyConfig) {
   let markdownLib = markdownIt({
     html: true,
     linkify: true,
-  }).use(markdownItLinkAttributes, {
-    matcher(href) {
-      return href.startsWith("http");
-    },
-    attrs: {
-      target: "_blank",
-      rel: "noopener noreferrer",
-    },
-  });
+  })
+    .use(markdownItLinkAttributes, {
+      matcher(href) {
+        return href.startsWith("http");
+      },
+      attrs: {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      },
+    })
+    .use(markdownItFootnote);
+
+  // Remove footnote brackets
+  markdownLib.renderer.rules.footnote_caption = (tokens, idx) => {
+    let n = Number(tokens[idx].meta.id + 1).toString();
+    if (tokens[idx].meta.subId > 0) {
+      n += ":" + tokens[idx].meta.subId;
+    }
+    return n;
+  };
+
+  // Change backref icon to up arrow
+  markdownLib.renderer.rules.footnote_anchor = (
+    tokens,
+    idx,
+    options,
+    env,
+    slf,
+  ) => {
+    let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
+
+    if (tokens[idx].meta.subId > 0) {
+      id += `:${tokens[idx].meta.subId}`;
+    }
+
+    return ` <a href="#fnref${id}" class="footnote-backref">↑</a>`;
+  };
 
   eleventyConfig.setLibrary("md", markdownLib);
 
